20809edfbd3fc83a3edcbf3ddd35b9262c213da4
Hannah Bannan <hannahbannan@MacBook-Pro.local>
2020-07-20 08:54:59 -0700

    components folder plus nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <h1>COVATING APP</h1>
+      <Nav />
     </div>
   );
 }
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <h3>This will be the nav!</h3>
+    )
+}
+
+export default Nav;

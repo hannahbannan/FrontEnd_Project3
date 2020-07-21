c7b8dfb8b2d6db17ffa0f604f0f5bc40a317fab4
naama <0naama0@gmail.com>
2020-07-21 15:15:19 -0400

    fixed merge issues

--- frontend/src/components/Register/RegisterForm.jsx
+++ frontend/src/components/Register/RegisterForm.jsx
@@ -44,90 +44,91 @@ const RegisterForm = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} encType="multipart/form-data" >
-      <label>What's your first name?</label>
-      <input
-        type="text"
-        value={input.firstName}
-        name="firstName"
-        onChange={handleChange}
-      />
+    <>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <label>What's your first name?</label>
+        <input
+          type="text"
+          value={input.firstName}
+          name="firstName"
+          onChange={handleChange}
+        />
+        <br />
+        <label>How old are you?</label>
+        <input
+          value={input.age}
+          name="age"
+          type="number"
+          min="18"
+          max="120"
+          onChange={handleChange}
+        />
+        <br />
+        <label>Where do you live?</label>
+        <input
+          type="text"
+          value={input.location}
+          name="location"
+          onChange={handleChange}
+        />
+        <br />
+        <label>What's your gender</label>
+        <input
+          type="text"
+          value={input.gender}
+          onChange={handleChange}
+          name="gender"
+        />
+        <br />
+        <label>What were your hobbies before COVID-19?</label>
+        <input
+          type="text"
+          value={input.hobbiesBefore}
+          name="hobbiesBefore"
+          onChange={handleChange}
+        />
+        <br />
+        <label>What were your hobbies after COVID-19?</label>
+        <input
+          type="text"
+          value={input.hobbiesAfter}
+          name="hobbiesAfter"
+          onChange={handleChange}
+        />
+        <br />
+        <label>Got COVID Antibodies?</label>
+        <input
+          type="radio"
+          value={input.antibodies}
+          name="antibodies"
+          value="yesAnti"
+          id="yes"
+          onChange={handleChange}
+        />
+        <label htmlFor="true">Yes</label>
 
-      <label>How old are you?</label>
-      <input
-        value={input.age}
-        name="age"
-        type="number"
-        min="18"
-        max="120"
-        onChange={handleChange}
-      />
-
-      <label>Where do you live?</label>
-      <input
-        type="text"
-        value={input.location}
-        name="location"
-        onChange={handleChange}
-      />
-
-      <label>What's your gender</label>
-      <input
-        type="text"
-        value={input.gender}
-        onChange={handleChange}
-        name="gender"
-      />
-
-      <label>What were your hobbies before COVID-19?</label>
-      <input
-        type="text"
-        value={input.hobbiesBefore}
-        name="hobbiesBefore"
-        onChange={handleChange}
-      />
-
-      <label>What were your hobbies after COVID-19?</label>
-      <input
-        type="text"
-        value={input.hobbiesAfter}
-        name="hobbiesAfter"
-        onChange={handleChange}
-      />
-
-      <label>Got COVID Antibodies?</label>
-      <input
-        type="radio"
-        value={input.antibodies}
-        name="antibodies"
-        value="true"
-        id="yes"
-        onChange={handleChange}
-      />
-      <label htmlFor="true">Yes</label>
-
-      <input
-        type="radio"
-        value={input.antibodies}
-        name="antibodies"
-        value="false"
-        id="no"
-        onChange={handleChange}
-      />
-      <label htmlFor="false">No</label>
-
-      <label>Upload a profile image</label>
-      <input
-        type="file"
-        value={input.image}
-        name="image"
-        onChange={handleChange}
-        accept="image/*"
-      />
-      <button type="submit">Create Profile</button>
-    </form>
+        <input
+          type="radio"
+          value={input.antibodies}
+          name="antibodies"
+          value="noAnti"
+          id="no"
+          onChange={handleChange}
+        />
+        <label htmlFor="false">No</label>
+        <br />
+        <label>Profile Image URL</label>
+        <input
+          type="text"
+          value={input.image}
+          name="image"
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Create Profile</button>
+      </form>
+    </>
   );
 };
-
 //test Image https://images.unsplash.com/photo-1552766985-80f8ade49ee9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80
 export default RegisterForm

d91ccc9366c760e8b18d6ab18362504e2fdfb9ba
naama <0naama0@gmail.com>
2020-07-22 17:45:54 -0400

    updated responsive css for home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Home.css'
+import "./Home.css";
 const Home = () => {
   return (
     <div className="home-page">
-      <h2>Antibody Can Join</h2>
+      <div className="form-sidebar">
+        <div className="antibody-tagline">Antibody Can Join</div>
+        <Link to="/register">
+          <button className="register-btn">Create A Profile</button>
+        </Link>
+      </div>
 
-      <Link to="/register">
-        <button className="register-btn">Create A Profile</button>
-      </Link>
+      <div className="main-vector">
+        <img
+          id="vector-img"
+          src="https://res.cloudinary.com/dzfyk3r12/image/upload/v1595450446/2857717-eps_2_s7l1hd.png"
+        ></img>
+      </div>
     </div>
   );
 };
